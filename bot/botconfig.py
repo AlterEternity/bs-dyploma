bb38145741c8e92e892892a4b9af597d84caf1c0
AlterEternity <kosmodenchik@icloud.com>
2019-03-14 19:26:33 +0200

    Added state to application

--- bot/botconfig.py
+++ bot/botconfig.py
@@ -1,1 +1,17 @@
+# Telegram token
+# TODO: discover options to hide value
 token = '660816078:AAG2GMt_u1J4akSGQETs-_Jf0f6qYuL4foE'
+
+
+# States
+class State(enumerate):
+    """
+    Class for defining states for all stages in dialogue
+    States: Start, Enter_event_name, Enter_date, Choose_event, Choose_tickets, Order_tickets
+    """
+    S_START = 0
+    S_ENTER_EVENT_NAME = 1
+    S_ENTER_DATE = 2
+    S_CHOOSE_EVENT = 3
+    S_CHOOSE_TICKETS = 4
+    S_ORDER_TICKETS = 5
--- bot/handlers.py
+++ bot/handlers.py
@@ -2,9 +2,14 @@ import telebot
 
 import botconfig
 
+# TODO: store state in DB
+default_state = botconfig.State.S_START
 bot = telebot.TeleBot(botconfig.token)
 
 
 @bot.message_handler(commands=['start', 'help'])
-def display_start(message):
+def display_start(message: str) -> int:
     bot.send_message(message.chat.id, "Hello World!")
+    current_state = botconfig.State.S_ENTER_EVENT_NAME
+    return current_state
+
