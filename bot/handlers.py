f5ecb55fc113f507e0df497b5bb17a251dc02320
AlterEternity <kosmodenchik@icloud.com>
2019-03-14 19:28:30 +0200

    removed hint for message as it isn't correct

--- bot/handlers.py
+++ bot/handlers.py
@@ -8,7 +8,7 @@ bot = telebot.TeleBot(botconfig.token)
 
 
 @bot.message_handler(commands=['start', 'help'])
-def display_start(message: str) -> int:
+def display_start(message) -> int:
     bot.send_message(message.chat.id, "Hello World!")
     current_state = botconfig.State.S_ENTER_EVENT_NAME
     return current_state
