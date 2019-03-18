f96fb86f68c0bed6a03e1a6f755d608dad9cbe0b
AlterEternity <kosmodenchik@icloud.com>
2019-03-18 16:39:55 +0200

    Create handler for /restart. Added global variables for current state and event info

--- bot/handlers.py
+++ bot/handlers.py
@@ -3,13 +3,34 @@ import telebot
 import botconfig
 
 # TODO: store state in DB
-default_state = botconfig.State.S_START
+DEFAULT_STATE = botconfig.State.S_START
+CURRENT_STATE = DEFAULT_STATE
+EVENT_INFO = dict()
+
 bot = telebot.TeleBot(botconfig.token)
 
 
 @bot.message_handler(commands=['start', 'help'])
-def display_start(message) -> int:
+def display_start(message):
+    """
+    Displays starting message for user
+    :param message:
+    """
     bot.send_message(message.chat.id, "Hello World!")
-    current_state = botconfig.State.S_ENTER_EVENT_NAME
-    return current_state
+    EVENT_INFO = {}
+    CURRENT_STATE = botconfig.State.S_ENTER_EVENT_NAME
+
+
+@bot.message_handler(commands=['restart'])
+def display_start(message):
+    """
+    Returning to start state
+    :param message:
+    """
+    bot.send_message(message.chat.id, "Okay...")
+    EVENT_INFO = dict()
+    CURRENT_STATE = DEFAULT_STATE
+
+
+
 
