36f1cd5811b2588a8ce3f2fa6fa65c3443595bac
AlterEternity <kosmodenchik@icloud.com>
2019-03-20 13:00:55 +0200

    doing some debugging

--- bot/handlers.py
+++ bot/handlers.py
@@ -2,12 +2,14 @@ import telebot
 
 from bot import botconfig
 
+
 # TODO: store state in DB
 DEFAULT_STATE = botconfig.State.S_START
 CURRENT_STATE = DEFAULT_STATE
-LANGUAGE = 'eng'
+LANGUAGE = None
 CHAT_ID = None
 EVENT_INFO = dict()
+
 
 bot = telebot.TeleBot(botconfig.TOKEN)
 
@@ -29,55 +31,51 @@ def display_start(message):
     CHAT_ID = message.chat.id
 
 
-@bot.callback_query_handler(func=lambda call: True)
+@bot.message_handler(commands=['restart'])
+def display_restart(message):
+    """
+    Returning to start state
+    :param message:
+    """
+    global EVENT_INFO, CURRENT_STATE
+    bot.send_message(message.chat.id, "Okay...")
+    EVENT_INFO = dict()
+    CURRENT_STATE = DEFAULT_STATE
+
+
+@bot.message_handler(func=lambda x: CURRENT_STATE == botconfig.State.S_ENTER_LANG)
+def get_event_name(message):
+    """
+    Getting event name to add it to dictionary
+    :param message:
+    """
+    print("I'm in")
+    bot.send_message(message.chat.id, "Please enter the name of event you want to go...")
+    EVENT_INFO.update({'name': message.text})
+    print(EVENT_INFO)
+
+
+@bot.message_handler(commands=['date'])
+def get_event_date(message):
+    print('here')
+    """
+    Getting event date to add it to dictionary
+    :param message:
+    """
+    pass
+
+
+@bot.callback_query_handler(func=lambda call: True)  # doesn't work
 def set_language(call):
     global LANGUAGE
     if call.message:
         LANGUAGE = call.data
 
 
-# For English language
-def dialogue():
-    global LANGUAGE, CURRENT_STATE, CHAT_ID
-    if LANGUAGE == 'eng':
-        print(LANGUAGE == 'eng')
-        print(CURRENT_STATE == botconfig.State.S_ENTER_LANG)
-        print("checking lang")
-        print(CHAT_ID)
-
-        @bot.message_handler(commands=['restart'])
-        def display_restart(message):
-            """
-            Returning to start state
-            :param message:
-            """
-            global EVENT_INFO, CURRENT_STATE
-            bot.send_message(message.chat.id, "Okay...")
-            EVENT_INFO = dict()
-            CURRENT_STATE = DEFAULT_STATE
-
-        @bot.message_handler(func=lambda CURRENT_STATE: True)
-        def get_event_name(message):
-            """
-            Getting event name to add it to dictionary
-            :param message:
-            """
-            print("I'm in")
-            bot.send_message(CHAT_ID, "Please enter the name of event you want to go...")
-            EVENT_INFO.update({'name': message.text})
-
-        @bot.message_handler(func=lambda x:CURRENT_STATE == botconfig.State.S_ENTER_EVENT_NAME)
-        def get_event_date(message):
-            """
-            Getting event date to add it to dictionary
-            :param message:
-            """
-            pass
 
 
-def check_value(a, b) -> bool:
-    if a == b:
-        return True
-    else:
-        return False
+
+
+
+
 
