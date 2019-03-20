2e1861f5759a3d9af6665453b48ba02acf790616
AlterEternity <kosmodenchik@icloud.com>
2019-03-20 19:11:23 +0200

    Added handler for getting date of event

--- bot/handlers.py
+++ bot/handlers.py
@@ -48,28 +48,36 @@ def get_event_name(message):
     Getting event name to add it to dictionary
     :param message:
     """
-    bot.send_message(message.chat.id, "Okay, saving it")
+    global CURRENT_STATE, EVENT_INFO
     EVENT_INFO.update({'name': message.text})
+    bot.send_message(CHAT_ID, "Okay, saving it")
+    bot.send_message(CHAT_ID, "Which date you want to attend?")
     print(EVENT_INFO)
+    CURRENT_STATE = botconfig.State.S_ENTER_EVENT_NAME
 
 
-@bot.message_handler(commands=['date'])
+@bot.message_handler(func=lambda x: CURRENT_STATE == botconfig.State.S_ENTER_EVENT_NAME)
 def get_event_date(message):
-    print('here')
     """
     Getting event date to add it to dictionary
     :param message:
     """
-    pass
+    global CURRENT_STATE, EVENT_INFO
+    EVENT_INFO.update({'date': message.text})
+    bot.send_message(CHAT_ID, "Great, let's check what you've got:")
+    bot.send_message(CHAT_ID, "Event name: " + EVENT_INFO.get('name') + "\n" + "Event date: " +
+                     EVENT_INFO.get('date'))
+    CURRENT_STATE = botconfig.State.S_ENTER_DATE
 
 
-@bot.callback_query_handler(func=lambda call: True)
+@bot.callback_query_handler(func=lambda x: CURRENT_STATE == botconfig.State.S_START)
 def set_language(call):
     global LANGUAGE, CURRENT_STATE, CHAT_ID
     if call.message:
         LANGUAGE = call.data
         bot.send_message(CHAT_ID, "Please enter the name of event you want to go...")
         CURRENT_STATE = botconfig.State.S_ENTER_LANG
+
 
 
 
